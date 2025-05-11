344689a2bfa34158fe0a8ca8593f06210dce9ec4
Batuhan <anhtubui574@gmail.com>
2025-05-11 14:44:09 +0700

    refactored tests to table-driven tests

--- KVS_test.go
+++ KVS_test.go
@@ -3,21 +3,52 @@ package Key_Value_Store
 import "testing"
 
 func TestKVS(t *testing.T) {
-	t.Run("Get non-exist key returns empty string and error", func(t *testing.T) {
-		kvs := NewInMemoryKVS()
-		got, err := kvs.Get("foo")
-		want := ""
-		assertEqual(t, got, want)
-		assertError(t, err)
-	})
-	t.Run("Put value and then Get returns value and nil", func(t *testing.T) {
-		kvs := NewInMemoryKVS()
-		err := kvs.Put("foo", "bar")
-		assertNoError(t, err)
-		got, err := kvs.Get("foo")
-		want := "bar"
-		assertEqual(t, got, want)
-	})
+	cases := []struct {
+		name      string
+		setup     func(kvs KeyValueStore)
+		key       string
+		wantValue string
+		wantErr   bool
+	}{
+		{
+			name:    "missing key",
+			setup:   func(kvs KeyValueStore) {},
+			key:     "foo",
+			wantErr: true,
+		},
+		{
+			name:      "single put",
+			setup:     func(kvs KeyValueStore) { kvs.Put("foo", "bar") },
+			key:       "foo",
+			wantValue: "bar",
+			wantErr:   false,
+		},
+		{
+			name: "overwrite key",
+			setup: func(kvs KeyValueStore) {
+				kvs.Put("foo", "bar")
+				kvs.Put("foo", "baz")
+			},
+			key:       "foo",
+			wantValue: "baz",
+			wantErr:   false,
+		},
+		// Add more cases ...
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			kvs := NewInMemoryKVS()
+			tc.setup(kvs)
+			got, err := kvs.Get(tc.key)
+			if tc.wantErr {
+				assertError(t, err)
+			} else {
+				assertNoError(t, err)
+				assertEqual(t, got, tc.wantValue)
+			}
+		})
+	}
 }
 
 func assertError(t *testing.T, err error) {
