d58bee6df2bdc2797c312fd3953aeaa88571473a
Batuhan <anhtubui574@gmail.com>
2025-05-11 18:01:22 +0700

    refactored tests

--- server_test.go
+++ server_test.go
@@ -11,34 +11,26 @@ type StubStore struct {
 	kv map[string]string
 }
 
-func (store StubStore) Get(key string) (string, error) {
+func (store *StubStore) Get(key string) (string, error) {
 	value := store.kv[key]
 	return value, nil
 }
 
-func (store StubStore) Put(key string, value string) error {
+func (store *StubStore) Put(key string, value string) error {
 	store.kv[key] = value
 	return nil
 }
 
 func TestGet(t *testing.T) {
 	t.Run("Get returns 404 on missing key", func(t *testing.T) {
-		store := &StubStore{}
-		server := NewServer(store)
+		server, response := newTestServer(map[string]string{})
 		request := httptest.NewRequest(http.MethodGet, "/kv/foo", nil)
-		response := httptest.NewRecorder()
 		server.ServeHTTP(response, request)
 		assertStatus(t, response.Code, http.StatusNotFound)
 	})
 	t.Run("Get on existing key return 200 and value", func(t *testing.T) {
-		store := &StubStore{
-			kv: map[string]string{
-				"foo": "bar",
-			},
-		}
-		server := NewServer(store)
+		server, response := newTestServer(map[string]string{"foo": "bar"})
 		request := httptest.NewRequest(http.MethodGet, "/kv/foo", nil)
-		response := httptest.NewRecorder()
 		server.ServeHTTP(response, request)
 		assertStatus(t, response.Code, http.StatusOK)
 		assertEqual(t, response.Body.String(), "bar")
@@ -47,23 +39,16 @@ func TestGet(t *testing.T) {
 
 func TestPut(t *testing.T) {
 	t.Run("Put returns 201", func(t *testing.T) {
-		store := &StubStore{
-			kv: map[string]string{},
-		}
-		server := NewServer(store)
+		server, response := newTestServer(map[string]string{})
 		request := httptest.NewRequest(http.MethodPost, "/kv/foo", strings.NewReader("bar"))
-		response := httptest.NewRecorder()
 		server.ServeHTTP(response, request)
 		assertStatus(t, response.Code, http.StatusAccepted)
 	})
 	t.Run("Put new value then Get return that value", func(t *testing.T) {
-		store := &StubStore{
-			kv: map[string]string{},
-		}
-		server := NewServer(store)
+		server, response := newTestServer(map[string]string{})
 		request := httptest.NewRequest(http.MethodPost, "/kv/foo", strings.NewReader("bar"))
-		response := httptest.NewRecorder()
 		server.ServeHTTP(response, request)
+
 		assertStatus(t, response.Code, http.StatusAccepted)
 
 		getRequest := httptest.NewRequest(http.MethodGet, "/kv/foo", nil)
@@ -72,6 +57,13 @@ func TestPut(t *testing.T) {
 		assertStatus(t, getResponse.Code, http.StatusOK)
 		assertEqual(t, getResponse.Body.String(), "bar")
 	})
+}
+
+func newTestServer(initial map[string]string) (*Server, *httptest.ResponseRecorder) {
+	store := &StubStore{kv: initial}
+	srv := NewServer(store)
+	response := httptest.NewRecorder()
+	return srv, response
 }
 
 func assertStatus(t testing.TB, got, want int) {
