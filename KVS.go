295a75e18150e524b91d2b7cce504b6a7e983029
Batuhan <anhtubui574@gmail.com>
2025-05-11 14:26:08 +0700

    added tests for Get operation

--- KVS.go
+++ KVS.go
@@ -1,16 +1,20 @@
 package Key_Value_Store
 
+import "errors"
+
 type KeyValueStore interface {
 	Put(key, value string) error
 	Get(key string) (string, error)
 }
 
 type InMemoryKVS struct {
-	store KeyValueStore
+	Store map[string]string
 }
 
 func NewInMemoryKVS() KeyValueStore {
-	return &InMemoryKVS{}
+	return &InMemoryKVS{
+		Store: make(map[string]string),
+	}
 }
 
 func (kvs *InMemoryKVS) Put(key, value string) error {
@@ -18,5 +22,8 @@ func (kvs *InMemoryKVS) Put(key, value string) error {
 }
 
 func (kvs *InMemoryKVS) Get(key string) (string, error) {
-	return "", nil
+	if val, ok := kvs.Store[key]; ok {
+		return val, nil
+	}
+	return "", errors.New("key not found")
 }
--- KVS_test.go
+++ KVS_test.go
@@ -3,12 +3,26 @@ package Key_Value_Store
 import "testing"
 
 func TestKVS(t *testing.T) {
-	t.Run("GET non-exist key returns empty string and nil", func(t *testing.T) {
-		store := NewInMemoryKVS()
-		got, _ := store.Get("foo")
+	t.Run("Get non-exist key returns empty string and error", func(t *testing.T) {
+		kvs := NewInMemoryKVS()
+		got, err := kvs.Get("foo")
 		want := ""
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertEqual(t, got, want)
+		assertError(t, err)
 	})
+	t.Run("Put value and then Get returns value and nil", func(t *testing.T) {})
+}
+
+func assertError(t *testing.T, err error) {
+	t.Helper()
+	if err == nil {
+		t.Fatalf("Expected to get error but get %v instead", err)
+	}
+}
+
+func assertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
